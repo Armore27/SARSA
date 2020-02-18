b315e81319488a94f3b434fad1ce2bb462642205
Armore27 <52496870+Armore27@users.noreply.github.com>
2020-02-19 00:15:03 +0300

    Update mirror_env.py

--- mirror_env.py
+++ mirror_env.py
@@ -7,41 +7,40 @@ class MirrorEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
+        self.done = False
         self.num_states = 0
-        self.num_actions = 27
         self.sg1 = 0
         self.sv1 = 0
         self.z1 = 0
         self.g1 = 0
         self.v1 = 0
         
-    def step(self):
+    def step(self, action):
         """
         This function determines possible actions.
         """
-        self.action = 0
         a = 10
         b = 1
         eng = matlab.engine.start_matlab()
 
-        for self.action in range(self.num_actions):
-            """
-            Useful actions, reward = -1.
-            """
-            if self.action == 0:
-               self.sg1 = eng.sdg1(a)
+        
+        """
+        Useful actions, reward = -1.
+        """
+        if action == 0:
+           self.sg1 = eng.sdg1(a)   #убрать табуляцию
 
-            if self.action == 1:
+            if action == 1:
                 self.sv1 = eng.sdv1(a)
 
             #if self.action == 2:
                 #self.z1 = eng.os1(a)
 
-            if self.action == 2:
+            if action == 2:
                 self.g1 = eng.gora1(b)
 
             if self.action == 3:
-                self.v1 = eng.vera1(b)
+                self.v1 = eng.vera1(b) #убрать self от actions
 
             if self.action == 4:
                 self.sg1 = eng.sdg1(a)
@@ -207,7 +206,7 @@ class MirrorEnv(gym.Env):
 
         self.state = self._get_state()
         reward = self._get_reward()
-        return self.state, reward, self.z1, self.sg1, self.sv1, self.g1, self.v1
+        return self.state, self.end, reward, self.z1, self.sg1, self.sv1, self.g1, self.v1
 
     def _get_state(self):
         """
@@ -223,14 +222,7 @@ class MirrorEnv(gym.Env):
         This function resets the environment into the initial state.
         """
         eng = matlab.engine.start_matlab()
-
-        #self.a_z1 = 0
-        self.a_sg1 = 0
-        self.a_sv1 = 0
-        self.b_g1 = 0
-        self.b_v1 = 0
-
-        #self.a_z1 = (-1)*self.z1
+       #self.a_z1 = (-1)*self.z1
         self.a_sg1 = (-1)*self.sg1
         self.a_sv1 = (-1)*self.sv1
         self.b_g1 = (-1)*self.g1
@@ -242,11 +234,11 @@ class MirrorEnv(gym.Env):
         self.g1 = eng.gora1(self.b_g1)
         self.g1 = eng.vera1(self.b_v1)
 
-        return self._get_state()
-
     def end(self):
         if self.num_states == 500:
-            return self.reset 
+           self.reset 
+           self.done = True
+        
 
     def _get_reward(self):
         """
